27ea5fe2fedb781f3d8be4670267b9884d16f57f
BuildTools <unconfigured@null.spigotmc.org>
2019-04-03 19:17:15 +0200

    Added Post for Positions

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
-from flask import Flask
 from flask_restful import Api, Resource, reqparse
-import json
-import pyowm
-import datetime as dt
 from math import sin, cos, sqrt, atan2, radians
+from flask import Flask
+import datetime as dt
+import requests
+import json
 import mpu
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,10 +13,19 @@ api = Api(app)
 MAPS_KEY = "5b3ce3597851110001cf62482e2890bc9f8e432886b7708caecd04e2"
 WEATHER_KEY = "0a187d268a9951e269589ac49f41a67c"
 
-# OWM docs https://media.readthedocs.org/pdf/pyowm/latest/pyowm.pdf
+WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
+ICON_URL = "http://openweathermap.org/img/w/"
+
 # GET für Koordinaten, Rechne entfernung zu POI aus
 # POST für standort schicken und in DB schreiben
 # TODO query from db to select all sites and load them into the python program
+# NOTE on export bring dependencies!
+
+
+""" Response Codes
+200 OK, 201 Created, 204 No Content
+400 Bad Request (Syntax), 401 Unauthorized (No Key), 403 Forbidden, 404 Not Found
+"""
 
 sites = [
     {
@@ -34,55 +44,69 @@ sites = [
     }
 ]
 
+users = [
+    {
+        "id": 1,
+        "name": "Dominic"
+    }
+]
+
 
 class Weather(Resource):
 
     """
     GET Weather at certain place
+    :return Returns a dict of the current weather conditions in a specified palce
+    If place is not specified Athens,GR will be used
     """
 
-    def get(self, place="Athens,GR"):
-        # You MUST provide a valid API key
-        owm = pyowm.OWM(WEATHER_KEY, language="de")
-        w = owm.weather_at_place(place).get_weather()
+    def get(self, place=None):
+        if place is None:
+            place = "Athens,GR"
 
-        d = {
-            "place": place,  # string
-            # Datetime Object
-            "time": w.get_reference_time("date").strftime("%x %X"),
-            # dict w/ cur max min
-            "temperature": w.get_temperature(unit="celsius"),
-            "cloud_percentage": w.get_clouds(),  # Number
-            "cloud_status": w.get_detailed_status()  # String e.g. bewölkt
-        }
+        res = requests.get(
+            WEATHER_URL, {"q": place, "appid": WEATHER_KEY, "units": "metric"})
 
-        return d
+        if res.status_code == 200:
+            res = res.json()  # Convert to JSON object to read data
 
-
-class AllSites(Resource):
-
-    # Returns all known sites as json
-    def get(self):
-        return json.dumps(sites), 201
-
-    # TODO def post(self, ARGS) to add new site
+            return {
+                "place": res["name"],
+                "date": dt.datetime.now().strftime("%X"),
+                "time": dt.datetime.now().strftime("%x"),
+                "min_temp": res["main"]["temp_min"],
+                "max_temp": res["main"]["temp_max"],
+                "temp": res["main"]["temp"],  # Temp in °C
+                "humidity": res["main"]["humidity"],  # Percentage
+                "icon": res["weather"][0]["icon"],  # Image ID for png
+                "description": res["weather"][0]["description"]
+            }
+        else:
+            return "Error: Getting weather from OpenWeatherMap failed!"
 
 
 class Sites(Resource):
 
+    """
+    Calculate the distance between two coordinates on a sphere
+    """
     @staticmethod
     def get_distance(lat1, lon1, lat2, lon2):
         return mpu.haversine_distance((lat1, lon1), (lat2, lon2))
 
     """
-    GET Request
+    GET request to either get all sites or sites in a certain radius from a coordinate
     :param x: X coordinate of the user
     :param y: Y coordinate of the user
     :return Returns a json object of the list of sites within a given radius of the users position
     Radius: Radius (kilometers) in which sites should be added to the result array
     """
 
-    def get(self, x, y, radius=5):
+    def get(self, x=None, y=None, radius=5):
+        if x is None and y is None:
+            return sites
+
+        print("Radius:", radius)
         sites_in_radius = []
         # TODO SQL query to get all sites
 
@@ -101,66 +125,46 @@ class Sites(Resource):
             return "Error: There are no points of interest within a radius of {}".format(radius), 404
 
 
-"""
-class User(Resource):
+class Position(Resource):
 
-    # Normale Get Anfrage, wenn der User existiert returne den User
+    """
+    Get position for Person
+    """
+
     def get(self, name):
-        for user in users:
-            if name == user["name"]:
-                return user, 200
-        return "User not found", 404
+        # TODO query to select newest positions for person
+        return "No positions found for {} ".format(name), 204
 
-    # POST Anfrage, erstellt neuen User mit Parametern oder gibt Fehler zurück wenn es ihn schon gibt
+    """
+    Write position to database under this name
+    """
+
     def post(self, name):
         parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
+        parser.add_argument("x")
+        parser.add_argument("y")
         args = parser.parse_args()
+
+        if args["x"] is None and args["y"] is None:
+            return "No coordinates specified!", 400
 
         for user in users:
             if name == user["name"]:
-                return "User with name {} already exists".format(name), 400
+                # TODO add new entry to Database here
+                print("New entry for {} added: ({}/{})".format(name,
+                                                               args["x"], args["y"]))
+                user["x"] = args["x"]
+                user["y"] = args["y"]
+                return user, 201
 
-        user = {
-            "name": name,
-            "age": args["age"],
-            "occupation": args["occupation"]
-        }
-
-        users.append(user)
-        return user, 201
-
-    # PUT ändert bereits existierenden User oder erstellt ihn neu
-    def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
-
-        for user in users:
-            if name == user["name"]:
-                user["age"] = args["age"]
-                user["occupation"] = args["occupation"]
-                return user, 200
-
-        user = {
-            "name": name,
-            "age": args["age"],
-            "occupation": args["occupation"]
-        }
-
-        users.append(user)
-        return user, 201
-
-    # Entfernt User
-    def delete(self, name):
-        global users
-        users = [user for user in users if user["name"] != name]
-        return "{} is deleted.".format(name), 200"""
+        # TODO Maybe create user if not exists
+        return "User does not exist!", 404
 
 
-api.add_resource(Sites, "/sites/<int:x>/<int:y>")
-api.add_resource(AllSites, "/allsites")
-api.add_resource(Weather, "/weather/<string:place>")
+# Can call /sites or /sites/x/y. First returns all sites, second sites in radius
+api.add_resource(Sites, "/sites", "/sites/<float:x>/<float:y>",
+                 "/sites/<float:x>/<float:y>/<float:radius>", endpoint="sites")
+api.add_resource(Weather, "/weather",
+                 "/weather/<string:place>", endpoint="weather")
+api.add_resource(Position, "/position/<string:name>")
 app.run(debug=True)
--- wetter.py
+++ wetter.py
@@ -1,12 +1,22 @@
-import pyowm
+import requests
 
+ATHENS_ID = 264371
 KEY = "0a187d268a9951e269589ac49f41a67c"
-
-owm = pyowm.OWM(KEY, language="de")  # You MUST provide a valid API key
-
+#URL = "api.openweathermap.org/data/2.5/weather?"
 # Search for current weather in London (Great Britain)
 #j = owm.get_json_at_place("London,GB")
-print(owm)
-observation = owm.weather_at_place('London,GB')
-w = observation.get_weather()
-print(w)
+URL = "https://api.openweathermap.org/data/2.5/weather"
+# Add imagecode and .png ie. /w/02d.png
+IMG_URL = "http://openweathermap.org/img/w/"
+
+# req = requests.get(URL, {"id": ATHENS_ID, "appid"})
+req = requests.get(
+    URL, {"q": "Athens,GR", "appid": KEY, "units": "metric"}).json()
+icon_id = req["weather"][0]["icon"]
+temperature = req["main"]["temp"]
+humidity = req["main"]["humidity"]
+weather = req["weather"]
+print(req)
+print(icon_id)
+print(temperature)
+print(humidity)
