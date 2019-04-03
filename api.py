9fceeee1842f71996f3de0ad759fd03918681a14
BuildTools <unconfigured@null.spigotmc.org>
2019-04-03 22:09:18 +0200

    Added Route support, api guide, various codes and cleaned up code

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask_restful import Api, Resource, reqparse
 from math import sin, cos, sqrt, atan2, radians
 from flask import Flask
+import mysql.connector
 import datetime as dt
 import requests
 import json
@@ -11,13 +12,14 @@ app = Flask(__name__)
 api = Api(app)
 
 MAPS_KEY = "5b3ce3597851110001cf62482e2890bc9f8e432886b7708caecd04e2"
-WEATHER_KEY = "0a187d268a9951e269589ac49f41a67c"
+MAPS_URL = "https://api.openrouteservice.org/v2/directions/foot-walking"
 
+WEATHER_KEY = "0a187d268a9951e269589ac49f41a67c"
 WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
 ICON_URL = "http://openweathermap.org/img/w/"
 
-# GET für Koordinaten, Rechne entfernung zu POI aus
-# POST für standort schicken und in DB schreiben
+ADMIN_KEY = "12345"
+
 # TODO query from db to select all sites and load them into the python program
 # NOTE on export bring dependencies!
 
@@ -30,14 +32,14 @@ ICON_URL = "http://openweathermap.org/img/w/"
 sites = [
     {
         "name": "Akropolis",
-        "adress": "Max Mustermannstrasse 1",
+        "address": "Max Mustermannstrasse 1",
         "x": 37.9715326,
         "y": 23.7257335,
         "description": "Das ist die Akropolis"
     },
     {
         "name": "Bonzata",
-        "adress": "Max Sonnleiten 21",
+        "address": "Max Sonnleiten 21",
         "x": 37.9215326,
         "y": 23.7557335,
         "description": "Das ist ein anderes Point of Interest!"
@@ -67,11 +69,11 @@ class Weather(Resource):
         res = requests.get(
             WEATHER_URL, {"q": place, "appid": WEATHER_KEY, "units": "metric"})
 
-        if res.status_code == 200:
-            res = res.json()  # Convert to JSON object to read data
-
+        code = res.status_code
+        res = res.json()  # Convert to JSON object to read data
+        if code == 200:
             return {
-                "place": res["name"],
+                "name": res["name"],
                 "date": dt.datetime.now().strftime("%X"),
                 "time": dt.datetime.now().strftime("%x"),
                 "min_temp": res["main"]["temp_min"],
@@ -82,7 +84,7 @@ class Weather(Resource):
                 "description": res["weather"][0]["description"]
             }
         else:
-            return "Error: Getting weather from OpenWeatherMap failed!"
+            return "Error: {}".format(res["message"]), res["cod"]
 
 
 class Sites(Resource):
@@ -103,15 +105,40 @@ class Sites(Resource):
     """
 
     def get(self, x=None, y=None, radius=5):
+        # If called sites without arguments
         if x is None and y is None:
-            return sites
+            parser = reqparse.RequestParser()
+            parser.add_argument("name")
+            args = parser.parse_args()
+
+            # If sites called and ?name is not found return all sites
+            if args["name"] is None:
+                return json.dumps(sites), 200
+
+            # Else return all sites with matching substrings
+            new_sites = []
+            for site in sites:
+                if args["name"].lower() in site["name"].lower():
+                    new_sites.append(site)
+
+            if len(new_sites) > 0:
+                return json.dumps(new_sites), 200
+            else:
+                return "Error: Site not found!", 404
+
+        parser = reqparse.RequestParser()
+        parser.add_argument("radius")
+        args = parser.parse_args()
+
+        if args["radius"] is not None:
+            radius = float(args["radius"])
 
         print("Radius:", radius)
         sites_in_radius = []
         # TODO SQL query to get all sites
 
         for site in sites:
-            # Distance in kilometers
+            # Get distance in kilometers and check if site is inside radius
             distance = self.get_distance(site["x"], site["y"], x, y)
             print(
                 "Distance between ({}/{}) and ({}/{}): {:.4f}km".format(site["x"], site["y"], x, y, distance))
@@ -120,9 +147,9 @@ class Sites(Resource):
                 sites_in_radius.append(site)
 
         if len(sites_in_radius) > 0:
-            return json.dumps(sites_in_radius, indent=4), 201
+            return json.dumps(sites_in_radius, indent=4), 200
         else:
-            return "Error: There are no points of interest within a radius of {}".format(radius), 404
+            return "No points of interest found within a radius of {}km".format(radius), 404
 
 
 class Position(Resource):
@@ -133,7 +160,7 @@ class Position(Resource):
 
     def get(self, name):
         # TODO query to select newest positions for person
-        return "No positions found for {} ".format(name), 204
+        return "No positions found for {}".format(name), 200
 
     """
     Write position to database under this name
@@ -149,22 +176,125 @@ class Position(Resource):
             return "No coordinates specified!", 400
 
         for user in users:
-            if name == user["name"]:
+            if name.lower() == user["name"].lower():
                 # TODO add new entry to Database here
                 print("New entry for {} added: ({}/{})".format(name,
                                                                args["x"], args["y"]))
                 user["x"] = args["x"]
                 user["y"] = args["y"]
+                user["date"] = dt.datetime.now().strftime("%x")
+                user["time"] = dt.datetime.now().strftime("%X")
                 return user, 201
 
         # TODO Maybe create user if not exists
         return "User does not exist!", 404
 
 
+class Route(Resource):
+
+    """
+    Get Route from point a to point b by foot
+    :param start Coordinate in format x.xx,y.yy
+    :param end Coordinate in format x.xx,y.yy
+    :return Returns the directions from A to B or Error 400
+    """
+
+    def get(self, start, end, write_to_file=False):
+        #start = "8.681495,49.41461"
+        #end = "8.687872,49.420318"
+
+        res = requests.get(
+            MAPS_URL, {"api_key": MAPS_KEY, "start": start, "end": end})
+
+        if res.status_code == 200:
+            res = res.json()
+
+            if write_to_file:  # TODO Remove later
+                with open("dir.json", "w") as f:
+                    json.dump(res, f, indent=4)
+
+            return res, 200
+        else:
+            return "Error: Route could not be calculated!\nError: {}".format(res.text()), 400
+
+
+class AdminSite(Resource):
+
+    def post(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("key")
+        parser.add_argument("address")
+        parser.add_argument("x")
+        parser.add_argument("y")
+        parser.add_argument("description")
+        args = parser.parse_args()
+
+        if args["key"] != ADMIN_KEY:
+            return "Error: Invalid key!", 401
+
+        if args["address"] is None or args["x"] is None or args["y"] is None or args["description"] is None:
+            return "Error: You must specify all variables!", 400
+
+        for site in sites:
+            if site["name"].lower() == name.lower():
+                # Site exists, change info that is different TODO alter table
+                site["address"] = args["address"]
+                site["x"]: float(args["x"])
+                site["y"]: float(args["y"])
+                site["description"]: args["description"]
+                return "{} updated successfully!".format(name), 200
+
+        # TODO database create new item
+        d = {
+            "name": name,
+            "address": args["address"],
+            "x": float(args["x"]),
+            "y": float(args["y"]),
+            "description": args["description"]
+        }
+        # append to array
+        return json.dumps(d), 200
+
+    def delete(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("key")
+        args = parser.parse_args()
+
+        if args["key"] != ADMIN_KEY:
+            return "Error: Invalid key!", 401
+
+        for site in sites:
+            if site["name"].lower() == name.lower():
+                # TODO remove from database
+                print("Removed ", name)
+
+                return "Removed {}!".format(name), 200
+
+        return "Error: Site not found!", 404
+
+
+"""
+Connect to all the databases needed for the program
+"""
+
+
+def db_connect():
+    cnx = mysql.connector.connect(
+        user="root", password="mysql#5BT", host="localhost", database="test")
+
+    return cnx
+    """cursor = cnx.cursor()
+
+    cursor.execute("CREATE TABLE test(vorname VARCHAR(255))")
+    cnx.close()"""
+
+
 # Can call /sites or /sites/x/y. First returns all sites, second sites in radius
-api.add_resource(Sites, "/sites", "/sites/<float:x>/<float:y>",
-                 "/sites/<float:x>/<float:y>/<float:radius>", endpoint="sites")
+api.add_resource(
+    Sites, "/sites", "/sites/<float:x>/<float:y>", endpoint="sites")
 api.add_resource(Weather, "/weather",
                  "/weather/<string:place>", endpoint="weather")
 api.add_resource(Position, "/position/<string:name>")
+api.add_resource(Route, "/route/<string:start>/<string:end>")
+api.add_resource(AdminSite, "/site/<string:name>")
 app.run(debug=True)
--- wetter.py
+++ wetter.py
@@ -1,22 +1,14 @@
 import requests
+import json
 
-ATHENS_ID = 264371
-KEY = "0a187d268a9951e269589ac49f41a67c"
-#URL = "api.openweathermap.org/data/2.5/weather?"
-# Search for current weather in London (Great Britain)
-#j = owm.get_json_at_place("London,GB")
-URL = "https://api.openweathermap.org/data/2.5/weather"
-# Add imagecode and .png ie. /w/02d.png
-IMG_URL = "http://openweathermap.org/img/w/"
+URL = 'https://api.openrouteservice.org/v2/directions/foot-walking'
+MAPS_KEY = "5b3ce3597851110001cf62482e2890bc9f8e432886b7708caecd04e2"
 
-# req = requests.get(URL, {"id": ATHENS_ID, "appid"})
-req = requests.get(
-    URL, {"q": "Athens,GR", "appid": KEY, "units": "metric"}).json()
-icon_id = req["weather"][0]["icon"]
-temperature = req["main"]["temp"]
-humidity = req["main"]["humidity"]
-weather = req["weather"]
-print(req)
-print(icon_id)
-print(temperature)
-print(humidity)
+start = "8.681495,49.41461"
+end = "8.687872,49.420318"
+
+res = requests.get(URL, {"api_key": MAPS_KEY, "start": start, "end": end})
+
+print(res.json())
+with open("dir.json", "w") as f:
+    json.dump(res.json(), f, indent=4)
