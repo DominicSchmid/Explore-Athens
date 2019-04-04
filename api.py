4f28808afc4cff59f34bbb151fdc7af309ad0fa1
Dominic <dominic.schmid@hotmail.com>
2019-04-04 12:43:13 +0200

    Added basic database support and impoved documentation

--- api.py
+++ api.py
@@ -1,5 +1,4 @@
 from flask_restful import Api, Resource, reqparse
-from math import sin, cos, sqrt, atan2, radians
 from flask import Flask
 import mysql.connector
 import datetime as dt
@@ -19,6 +18,7 @@ WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
 ICON_URL = "http://openweathermap.org/img/w/"
 
 ADMIN_KEY = "12345"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 # TODO query from db to select all sites and load them into the python program
 # NOTE on export bring dependencies!
@@ -97,25 +97,27 @@ class Sites(Resource):
         return mpu.haversine_distance((lat1, lon1), (lat2, lon2))
 
     """
-    GET request to either get all sites or sites in a certain radius from a coordinate
-    :param x: X coordinate of the user
-    :param y: Y coordinate of the user
-    :return Returns a json object of the list of sites within a given radius of the users position
+    Gets all sites or sites in a certain radius from a given coordinate
+    :param x: X coordinate of the user NOTE: Must be a float eg. 2.0
+    :param y: Y coordinate of the user NOTE: Must be a float eg. 2.0
+    :return Returns a json object of the list of sites within a given radius of the users position. Defaults to 5 kilometers
     Radius: Radius (kilometers) in which sites should be added to the result array
     """
 
     def get(self, x=None, y=None, radius=5):
+        if len(sites) == 0:  # If sites array is empty renew. This should really only happen atthe beginning
+            renew_sites()
         # If called sites without arguments
         if x is None and y is None:
             parser = reqparse.RequestParser()
             parser.add_argument("name")
             args = parser.parse_args()
 
-            # If sites called and ?name is not found return all sites
+            # If sites called and no name is given then return all sites
             if args["name"] is None:
                 return json.dumps(sites), 200
 
-            # Else return all sites with matching substrings
+            # Else return all sites with matching name substrings
             new_sites = []
             for site in sites:
                 if args["name"].lower() in site["name"].lower():
@@ -126,16 +128,16 @@ class Sites(Resource):
             else:
                 return "Error: Site not found!", 404
 
+        # Otherwise the request was for a radius calculation
         parser = reqparse.RequestParser()
         parser.add_argument("radius")
         args = parser.parse_args()
 
+        # Use specified radius if radius is given
         if args["radius"] is not None:
             radius = float(args["radius"])
 
-        print("Radius:", radius)
         sites_in_radius = []
-        # TODO SQL query to get all sites
 
         for site in sites:
             # Get distance in kilometers and check if site is inside radius
@@ -156,11 +158,14 @@ class Position(Resource):
 
     """
     Get position for Person
+    :return Returns a JSON Object of the user and his last position
     """
 
     def get(self, name):
-        # TODO query to select newest positions for person
-        return "No positions found for {}".format(name), 200
+        position = get_last_position(name)
+        if position is None:
+            return "User '{}' does not exist or did not submit a position yet!".format(name), 404
+        return json.dumps(position), 200
 
     """
     Write position to database under this name
@@ -175,19 +180,11 @@ class Position(Resource):
         if args["x"] is None and args["y"] is None:
             return "No coordinates specified!", 400
 
-        for user in users:
-            if name.lower() == user["name"].lower():
-                # TODO add new entry to Database here
-                print("New entry for {} added: ({}/{})".format(name,
-                                                               args["x"], args["y"]))
-                user["x"] = args["x"]
-                user["y"] = args["y"]
-                user["date"] = dt.datetime.now().strftime("%x")
-                user["time"] = dt.datetime.now().strftime("%X")
-                return user, 201
-
+        user = get_user(name)
+        if user is None:
+            return "Error: User does not exist!", 404
         # TODO Maybe create user if not exists
-        return "User does not exist!", 404
+        return add_position(user, args["x"], args["y"])
 
 
 class Route(Resource):
@@ -213,9 +210,9 @@ class Route(Resource):
                 with open("dir.json", "w") as f:
                     json.dump(res, f, indent=4)
 
-            return res, 200
+            return json.dumps(res, indent=4), 200
         else:
-            return "Error: Route could not be calculated!\nError: {}".format(res.text()), 400
+            return "Error: Route could not be calculated!\nError: {}".format(res.text), 400
 
 
 class AdminSite(Resource):
@@ -274,22 +271,133 @@ class AdminSite(Resource):
 
 
 """
-Connect to all the databases needed for the program
+Connects to database
+:return Returns an instance of a MySQL Connection
 """
 
 
 def db_connect():
     cnx = mysql.connector.connect(
-        user="root", password="mysql#5BT", host="localhost", database="test")
+        user="root", password="mysql#5BT", host="localhost", database="python", auth_plugin="mysql_native_password")
 
     return cnx
-    """cursor = cnx.cursor()
-
-    cursor.execute("CREATE TABLE test(vorname VARCHAR(255))")
-    cnx.close()"""
 
 
-# Can call /sites or /sites/x/y. First returns all sites, second sites in radius
+"""
+Updates site array with newest sites by fetching all from mysql database
+"""
+
+
+def renew_sites():
+    cnx = db_connect()
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM sites")
+
+    # Update all known sites
+    sites.clear()
+    for entry in cursor.fetchall():
+        # Name, Address, X, Y, Description
+        sites.append({
+            "name": entry[1],
+            "address": entry[2],
+            "x": entry[3],
+            "y": entry[4],
+            "description": entry[5]
+        })
+    cnx.close()  # Close due to resource leakage
+
+
+"""
+Gets last known position of a user by name
+:return A dictionary of the user and the last known coordinates, together with a timestamp
+"""
+
+
+def get_last_position(name):
+    cnx = db_connect()
+    cursor = cnx.cursor()
+    mysql_f = "%Y-%m-%d %H:%M:%S"  # Format of MYSQL dates
+
+    vals = (name,)  # Create tuple to prevent injection
+    cursor.execute(
+        "SELECT * FROM users JOIN positions ON users.u_id = positions.p_id WHERE kuerzel LIKE %s", vals)
+
+    res = cursor.fetchall()
+    cnx.close()  # Close due to resource leakage
+    if res is None or len(res) == 0:
+        return None
+
+    entry = res[len(res) - 1]  # Get last position TODO check if this works
+
+    if entry[6] is None or entry[7] is None:  # TODO check if i need to do this or if it worx
+        return "No poitions", 404
+
+    return {
+        "kuerzel": entry[1],
+        "vorname": entry[2],
+        "nachname": entry[3],
+        "x": entry[6],
+        "y": entry[7],
+        "dt": dt.datetime.strptime(str(entry[8]), mysql_f).strftime(DATE_FORMAT)
+    }
+
+
+"""
+Get user object by name eg. stschdom
+:param name The username for a given user
+:return Returns a dictionary with the user id, name, firstname and lastname or None if he does not exist
+"""
+
+
+def get_user(name):
+    cnx = db_connect()
+    cursor = cnx.cursor()
+
+    cursor.execute(
+        "SELECT * FROM users WHERE kuerzel LIKE %s", (name,))
+
+    res = cursor.fetchall()
+    cnx.close()  # Close due to resource leakage
+    if res is None or len(res) == 0:
+        return None
+
+    # Get last entry TODO check if this works and if i need this
+    entry = res[len(res) - 1]
+
+    return {
+        "id": entry[0],
+        "kuerzel": entry[1],
+        "vorname": entry[2],
+        "nachname": entry[3]
+    }
+
+
+"""
+Add new position for a given user
+:param user The user Object containing id, name, firstname and lastname
+:param x The new x coordinate
+:param y The new y coordinate
+:return Returns A string and HTTP status code for answering the API request
+"""
+
+
+def add_position(user, x, y):
+    cnx = db_connect()
+    cursor = cnx.cursor()
+
+    try:
+        vals = (user["id"], x, y, dt.datetime.now().strftime(DATE_FORMAT))
+        cursor.execute(
+            "INSERT INTO positions (p_uid, p_x, p_y, p_dt) VALUES (%s, %s, %s, %s)", vals)
+
+        cnx.commit()
+        cnx.close()  # Close due to resource leakage
+        return "Position added successfully!", 201
+    except:
+        return "Error: Syntax error inserting new position for {}!".format(user), 400
+
+
+# You must call as a float 2.0 or you will get a 404 error
 api.add_resource(
     Sites, "/sites", "/sites/<float:x>/<float:y>", endpoint="sites")
 api.add_resource(Weather, "/weather",
--- wetter.py
+++ wetter.py
@@ -1,14 +1,54 @@
-import requests
-import json
+import mysql.connector
+import datetime as dt
 
-URL = 'https://api.openrouteservice.org/v2/directions/foot-walking'
-MAPS_KEY = "5b3ce3597851110001cf62482e2890bc9f8e432886b7708caecd04e2"
+cnx = mysql.connector.connect(
+    user="root", password="mysql#5BT", host="localhost", database="python", auth_plugin="mysql_native_password")
 
-start = "8.681495,49.41461"
-end = "8.687872,49.420318"
 
-res = requests.get(URL, {"api_key": MAPS_KEY, "start": start, "end": end})
+mysql_f = "%Y-%m-%d %H:%M:%S"
 
-print(res.json())
-with open("dir.json", "w") as f:
-    json.dump(res.json(), f, indent=4)
+cursor = cnx.cursor()
+vals = ("%" + "stschdom" + "%",)
+cursor.execute(
+    "SELECT * FROM users JOIN positions ON users.u_id = positions.p_id WHERE kuerzel LIKE %s", vals)
+
+res = cursor.fetchall()
+cnx.close()  # Close due to resource leakage
+if res is None:
+    print("None")
+
+entry = res[len(res) - 1]  # Get last position
+print(entry)
+
+d = {
+    "kuerzel": entry[1],
+    "vorname": entry[2],
+    "nachname": entry[3],
+    "x": entry[6],
+    "y": entry[7],
+    "dt": dt.datetime.strptime(str(entry[8]), mysql_f).strftime("%x %X")
+}
+print(d)
+cnx.close()
+
+
+def write_to_db(db, song):
+    cursor = db.cursor()
+    # datetime.datetime.strptime(song["date"], "%c").strftime("%Y-%m-%d %H:%M:%S")
+    newdate = None
+
+    cnx = db_connect()
+    cursor = cnx.cursor()
+
+    """try:
+        sql = INSERT INTO spotify(artist, song, date, duration, explicit)
+        VALUES (%s, %s, %s, %s, %s)
+        vals = (song["artist"], song["song"], newdate,
+                song["duration_ms"], song["explicit"])
+        cursor.execute(sql, vals)
+        db.commit()
+        cursor.close()
+        # time.sleep(0.5)
+    except Exception as e:
+        print(e)
+        print("Failed inserting song")"""
